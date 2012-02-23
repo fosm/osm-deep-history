5a9292ca588176d7228b74f51d7c7a8dab6ead89
James Michael DuPont <JamesMikeDuPont@gmail.com>
2012-02-23 09:01:09 -0800

    adjusted for fosm

--- node.php
+++ node.php
@@ -7,22 +7,30 @@ if(!is_numeric($_GET['id'])) {
 }
 
 $id = $_GET['id'];
+$server = $_GET['server'];
 
+if ($server == 'fosm') {
+$url = "http://fosm.org/api/0.6/node/$id/history";
+}
+else
+{
 $url = "http://www.openstreetmap.org/api/0.6/node/$id/history";
+}
 
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_USERAGENT, "curl/deep_history_viewer (http://osm.mapki.com/history/)");
-$output = curl_exec($ch);
+//$ch = curl_init();
+//curl_setopt($ch, CURLOPT_URL, $url);
+//curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+//curl_setopt($ch, CURLOPT_USERAGENT, "curl/deep_history_viewer (http://osm.mapki.com/history/)");
+$output =  file_get_contents($url);
 
-$http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+//$http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 
+/*
 if($http_code != 200) {
   print "Error retrieving history: $http_code";
   exit;
 }
-
+*/
 $xml = simplexml_load_string($output);
 
 $nodes = array();
